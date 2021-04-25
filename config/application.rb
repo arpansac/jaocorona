fe5d61a2bf3666547ab2152afa3bd77b6c64b005
arpansac <arpansac@gmail.com>
2021-04-25 19:19:55 +0530

    remove extra request methods from cors

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module Jaocoronaapp
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options ,:delete ,:put]
+        resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
 
